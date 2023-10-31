12523d3cc6119b2eec0b0aebc69f2fc422d77142
Sasha Ogurtov <sasha@surfly.com>
2023-10-31 15:07:59 +0100

    add vulnerability script

---
+++ dependabot-vulnerabilities.l.3600s.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+
+# Set the GitHub access token
+# You can generate a personal access token at https://github.com/settings/tokens
+access_token=""
+
+# Define the repositories to check
+repositories=(
+    "surfly/cobro"
+    # "surfly/deployment"
+    # "surfly/install"
+    # "surfly/session-recording"
+    # "surfly/ci"
+)
+
+# Initialize the total number of pull requests to 0
+total_vulnerabilities=0
+
+# Loop through the repositories and add up the number of pending vulnerabilities
+for repository in "${repositories[@]}"; do
+    url="https://api.github.com/repos/$repository/dependabot/alerts?severity=critical&state=open"
+    vulnerabilities=$(curl -s -H "Authorization: token $access_token" "$url" | jq -r ". | length")
+    total_vulnerabilities=$((total_pull_requests + vulnerabilities))
+done
+
+# Print the total number of pull requests
+if [ "$total_vulnerabilities" -gt 0 ]; then
+    echo "$total_vulnerabilities | color=#FF0000 image=$(curl -s "https://github.githubassets.com/assets/dependabot-icon-3f88d04d37bc.png" | base64 -w 0) imageWidth=20"
+else
+    echo "| color=#00FF00 image=$(curl -s "https://github.githubassets.com/assets/dependabot-icon-3f88d04d37bc.png" | base64 -w 0) imageWidth=20"
+fi
+
+echo "---"
+echo "View Vulnerabilities | href=https://github.com/surfly/cobro/security/dependabot?q=is%3Aopen+severity%3Acritical"
